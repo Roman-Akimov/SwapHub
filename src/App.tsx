6b98a772de11ee86147f72516feb50b5f654a621
roman <168015073+Roman-Akimov@users.noreply.github.com>
2026-01-06 20:35:29 +0400

    Небольшая планировка и примерный разбор сервиса

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+type Good = {
+  id: number,
+  name: string,
+  price: number,
+  currency: string,
+  image: string,
+  description: string
+};
 
-function App() {
-  const [count, setCount] = useState(0)
+export const App = () => {
+  const goods: Good[] = [
+    { id: 1,
+      name: "Iphone",
+      price: 70, 
+      currency: "$", 
+      image: "https://avatars.mds.yandex.net/i?id=7165e767da4f62e966db16f3cd9ae01c568d1fbe-5266118-images-thumbs&n=13",
+      description: "Iphone 10"
+    },
+    { id: 1,
+      name: "Iphone",
+      price: 80, 
+      currency: "$", 
+      image: "https://avatars.mds.yandex.net/i?id=7165e767da4f62e966db16f3cd9ae01c568d1fbe-5266118-images-thumbs&n=13",
+      description: "Iphone 11"
+    },
+    { id: 1,
+      name: "Iphone",
+      price: 90, 
+      currency: "$", 
+      image: "https://avatars.mds.yandex.net/i?id=7165e767da4f62e966db16f3cd9ae01c568d1fbe-5266118-images-thumbs&n=13",
+      description: "Iphone 12"
+    }
+  ]
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1></h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <h1>Swap Hub</h1>
 
-export default App
+      <div>
+        {goods.map((item) => (
+          <div key={item.id}>
+            <h2>{item.name}</h2>
+            <p>{item.price}{item.currency}</p>
+            <img src={item.image} alt="good" />
+            <p>{item.description}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+
+};
