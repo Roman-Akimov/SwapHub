7b7227b51db5919b84595271703b367dc33c86d8
roman <168015073+Roman-Akimov@users.noreply.github.com>
2026-02-01 20:56:17 +0400

    #Проверка работы репы

--- webswaphub/src/lib/trpc.tsx
+++ webswaphub/src/lib/trpc.tsx
@@ -18,6 +18,10 @@ export const queryClient = new QueryClient({
   },
 });
 
+const number: number = 12;
+// eslint-disable-next-line no-console
+console.log(number);
+
 export const trpcClient = trpc.createClient({
   links: [
     // links в trpc - это аналог middleware
