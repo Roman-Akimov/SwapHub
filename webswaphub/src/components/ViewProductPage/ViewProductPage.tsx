c4d75dbaa083f22e6e62be1e94b5f688c4c6d38f
roman <168015073+Roman-Akimov@users.noreply.github.com>
2026-02-04 16:36:37 +0400

    #Написал функцию для работы с routes

--- webswaphub/src/components/ViewProductPage/ViewProductPage.tsx
+++ webswaphub/src/components/ViewProductPage/ViewProductPage.tsx
@@ -1,5 +1,5 @@
 import { useParams } from 'react-router-dom';
-import type { ViewProductRouteParams } from '../../lib/routes';
+import { type ViewProductRouteParams } from '../../lib/routes';
 
 export const ViewProductPage = () => {
   const { productName } = useParams() as ViewProductRouteParams;
--- webswaphub/src/lib/routes.ts
+++ webswaphub/src/lib/routes.ts
@@ -1,8 +1,28 @@
-export const viewProductRouteParams = { productName: ':productName' };
-export type ViewProductRouteParams = { productName: string };
+/** Можно написать лучше, без boolean. По возможности, отрефакторить */
+const getRouteParams = <T extends Record<string, boolean>>(object: T) => {
+  return Object.keys(object).reduce(
+    (acc, key) => {
+      return { ...acc, [key]: `:${key}` };
+    },
+    {} // "начальное значение аккумулятора, он явл. объектом"
+  ) as Record<keyof T, string>;
+};
+
+export const viewProductRouteParams = getRouteParams({ productName: true });
+
+export type ViewProductRouteParams = typeof viewProductRouteParams;
+
+export const getViewProductPage = ({ productName }: ViewProductRouteParams) => {
+  return `/products/${productName}`;
+};
+
 export const getAllProductsPage = () => {
   return '/';
 };
-export const getViewProductPage = ({ productName }: { productName: string }) => {
-  return `/products/${productName}`;
-};
+
+// export const viewProductRouteParams = { productName: ':productName' };
+// export type ViewProductRouteParams = { productName: string };
+
+// export const getViewProductPage = ({ productName }: { productName: string }) => {
+//   return `/products/${productName}`;
+// };
