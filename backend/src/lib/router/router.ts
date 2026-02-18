23fa430d08ff7380eb4cc343db4622613dc6230c
roman <168015073+Roman-Akimov@users.noreply.github.com>
2026-02-18 12:50:05 +0400

    #Попытка поставить test-playgruond

--- backend/src/lib/router/router.ts
+++ backend/src/lib/router/router.ts
@@ -2,11 +2,13 @@ import { app } from '../trpc/trpc';
 import { createProductTrpcRoute } from './createProduct/createProduct';
 import { getProductTrpcRoute } from './getProduct/getProduct';
 import { getProductsTrpcRoute } from './getProducts/getProducts';
+import { signUpTrpcRoute } from './signUp/signUp.endpoint';
 
 export const appRouter = app.router({
   getProduct: getProductTrpcRoute,
   getProducts: getProductsTrpcRoute,
   createProduct: createProductTrpcRoute,
+  signUp: signUpTrpcRoute
 });
 
 export type AppRouter = typeof appRouter;
